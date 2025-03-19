82a76df14135ae9f1eb9e4aab87de966f80aa345
Agata Kiełb <agata.malgorzata.kielb@gmail.com>
2025-03-19 21:55:14 +0100

    Some tkinter

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from win32gui import GetForegroundWindow
 from win32process import GetWindowThreadProcessId
 import pandas as pd
 import psutil
-
+import tkinter as tk
+from tkinter import ttk
 class Evil_Pomodoro():
     def __init__(self):
         self.allowed_windows = []
@@ -49,6 +50,36 @@ class Evil_Pomodoro():
     def main(self):
         pass
 
+def submit_time():
+    time = time_var.get()
+    print(time)
+
 if __name__ == "__main__":
     pomodoro = Evil_Pomodoro()
     pomodoro.main()
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.geometry("350x350")
+    root.title("Panopticon Pomodoro")
+
+    frame = tk.ttk.Frame(root)
+
+    background_image = tk.PhotoImage(file=".\\art\\background.png")
+    background_label = tk.Label(root, image=background_image)
+    background_label.place(x=0, y=0, relheight=1, relwidth=1)
+    
+    quit_btn = tk.Button(
+        root,
+        text="Quit",
+        command=root.destroy
+    )
+    time_var = tk.StringVar()
+    time_entry = tk.Entry(root, textvariable=time_var)
+    default_work = time_entry.insert(-1, "25")
+    start_btn = tk.Button(root, text="Start Work", command= submit_time)
+    
+    
+    quit_btn.grid(row=0, column=0)
+    start_btn.grid(row=1, column=0)
+    time_entry.grid(row=2, column=0)
+    root.mainloop()
